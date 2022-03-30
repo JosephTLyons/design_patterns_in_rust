c42e5daf4685d79583d0ce1acd40f4a0ac0c0f38
Joseph T Lyons <JosephTLyons@gmail.com>
2022-03-30 16:13:24 -0400

    Add state pattern example

---
+++ state_pattern/src/main.rs
@@ -0,0 +1,58 @@
+struct EngineOff {}
+
+impl EngineOff {
+    fn engine_on(self) -> Idle {
+        Idle {}
+    }
+}
+
+struct Idle {}
+
+impl Idle {
+    fn forward(self) -> Forward {
+        Forward {}
+    }
+
+    fn backward(self) -> Backward {
+        Backward {}
+    }
+
+    fn engine_off(self) -> EngineOff {
+        EngineOff {}
+    }
+}
+
+trait Drive {
+    fn stop(self) -> Idle
+    where
+        Self: Sized,
+    {
+        Idle {}
+    }
+    fn left(&self) {}
+    fn right(&self) {}
+}
+
+struct Forward {}
+impl Drive for Forward {}
+
+struct Backward {}
+impl Drive for Backward {}
+
+fn main() {
+    let car = EngineOff {};
+    let car = car.engine_on();
+    let car = car.forward();
+
+    car.left();
+    car.right();
+
+    let car = car.stop();
+    let car = car.backward();
+
+    car.left();
+    car.right();
+
+    let car = car.stop();
+    let _ = car.engine_off();
+}
